f99af5b4eb901853f1c2a7e9e22ae9c9c0011b1f
Axel Séguret <axelseguret@gmail.com>
2014-09-22 18:08:51 +0200

    solution1

---
+++ exercices/019/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 15:26:16 2014
+
+@author: Axel
+"""
+import sys
+if len(sys.argv)>1:
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+    print(a + b)
+else:
+    print("usage: python3 solution.py OP1 OP2")
