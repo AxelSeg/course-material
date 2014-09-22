8c3685096b54d6d7479a4c9de8ec8ee342b25b1d
Axel Séguret <axelseguret@gmail.com>
2014-09-22 18:10:01 +0200

    solution1

---
+++ exercices/013/solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 15:26:16 2014
+
+@author: Axel
+"""
+import sys
+if len(sys.argv)>1
+    print(sys.argv[0])
+else:
+    print(usage: python3 solution.py PARAM)
