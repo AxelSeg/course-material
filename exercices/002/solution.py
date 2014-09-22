bf7b76cb226714ee04124f3608ed1f3b58a4f58e
Axel Séguret <axelseguret@gmail.com>
2014-09-22 11:38:50 +0200

    solution1

---
+++ exercices/002/solution.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 10:42:41 2014
+
+@author: Axel
+"""
+
+a = ('abcdefghijklnmopqrstuvwxyz')
+b = a[::-1]
+#print(b)
+indices = set([5,11,17,21,25])
+d = print("".join(c.upper() if i in indices else c for i, c in enumerate(b)))
