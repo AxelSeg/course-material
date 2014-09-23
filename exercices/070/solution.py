85ef09809c79d0979223f8d833479759cea25147
Axel Séguret <axelseguret@gmail.com>
2014-09-23 12:16:47 +0200

    solution2

--- exercices/070/solution.py
+++ exercices/070/solution.py
@@ -5,8 +5,8 @@ Created on Mon Sep 22 22:37:07 2014
 @author: Axel
 """
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-for i in range (0, 25):
-    for j in range (0, 25):
+for i in range (0, 26):
+    for j in range (0, 26):
         if alphabet[i] != alphabet[j]:
             print(alphabet[i]+alphabet[j])
         else:
