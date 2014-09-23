862dfb925a1855c6b0520a05b1ad06dfe4d772e2
Axel Séguret <axelseguret@gmail.com>
2014-09-23 18:10:51 +0200

    solution1

---
+++ exercices/200/solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 22:37:07 2014
+
+@author: Axel
+"""
+def is_prime(num):
+    if num % 2 == 0:
+        print(True)
+    else:
+        print(False)
