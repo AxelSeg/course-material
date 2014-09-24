89b0aa9befc76e85e2daa68040348f21d97aa96f
Axel Séguret <axelseguret@gmail.com>
2014-09-24 11:26:11 +0200

    solution1

---
+++ exercices/203/solution.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 22 22:37:07 2014
+
+@author: Axel
+"""
+def is_multiple(value1, value2):
+    if value2 % value1 == 0:
+        print(True)
+    else:
+        print(False)
+
+is_multiple(2, 6)
