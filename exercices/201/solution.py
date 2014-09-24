cddd91230c13fdae79772a0cd873cc9c52d886fb
Axel Séguret <axelseguret@gmail.com>
2014-09-24 10:44:24 +0200

    solution1

--- exercices/201/solution.py
+++ exercices/201/solution.py
@@ -4,10 +4,8 @@ Created on Mon Sep 22 22:37:07 2014
 
 @author: Axel
 """
-def is_alpha(input):
-    elif input != "[0-9]":
-        print(False)
-    else:
+def is_alpha(letter):
+    if letter.isalpha() == True:
         print(True)
-
-is_alpha(5)
+    else:
+        print(False)
