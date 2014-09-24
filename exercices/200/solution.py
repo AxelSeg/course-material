d1e7ac6a637e2b2126233a477d48843d8fa3fe01
Axel Séguret <axelseguret@gmail.com>
2014-09-24 13:48:21 +0200

    solution1

--- exercices/200/solution.py
+++ exercices/200/solution.py
@@ -5,7 +5,8 @@ Created on Mon Sep 22 22:37:07 2014
 @author: Axel
 """
 def is_prime(num):
-    if num % 2 == 0:
-        print(True)
-    else:
-        print(False)
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+        else:
+            return True
