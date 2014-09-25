6c8cd331b15e6f23212dbf18fa4b28dee824d25a
Axel Séguret <axelseguret@gmail.com>
2014-09-25 13:35:11 +0200

    solution1

--- exercices/250/solution.py
+++ exercices/250/solution.py
@@ -4,10 +4,13 @@ Created on Mon Sep 22 22:37:07 2014
 
 @author: Axel
 """
+
+
 def draw_n_squares(n):
     hor = '+---'
     ver = '|   '
     plus = '+'
     for i in range(n):
-        print(hor * n + plus), print(ver * (n+1))
+        print(hor * n + plus)
+        print(ver * (n + 1))
     print(hor * n + plus)
