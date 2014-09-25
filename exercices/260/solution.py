c662318c1c9169c39a724fab89b7af9626e95392
Axel Séguret <axelseguret@gmail.com>
2014-09-25 13:34:38 +0200

    solution2

--- exercices/260/solution.py
+++ exercices/260/solution.py
@@ -15,20 +15,14 @@ def euclidean(a, b):
     print(d)
     return d
 
-a = [2,3,7,3,4,0,8,2,1]
-b = [5,3,0,7,8,6,9,4,5]
-euclidean(a, b)
 
 def opt_euclidean(a, b):
     d = math.sqrt(sum(math.pow((b - a), 2) for a, b in zip(a, b)))
     print(d)
     return d
 
-opt_euclidean(a, b)
 
 def np_euclidean(a, b):
     d = np.sqrt(sum(np.power((b - a), 2) for a, b in zip(a, b)))
     print(d)
     return d
-np_euclidean(a, b)
-
