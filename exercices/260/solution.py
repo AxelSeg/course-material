0fa6d884109f03f287a44cb1065c34051bc5c155
Axel Séguret <axelseguret@gmail.com>
2014-09-25 13:33:34 +0200

    solution1

--- exercices/260/solution.py
+++ exercices/260/solution.py
@@ -6,11 +6,29 @@ Created on Mon Sep 22 22:37:07 2014
 """
 
 
-def draw_n_squares(n):
-    hor = '+---'
-    ver = '|   '
-    plus = '+'
-    for i in range(n):
-        print(hor * n + plus)
-        print(ver * (n + 1))
-    print(hor * n + plus)
+import math
+import numpy as np
+
+
+def euclidean(a, b):
+    d = sum((b - a)**2 for a, b in zip(a, b))**0.5
+    print(d)
+    return d
+
+a = [2,3,7,3,4,0,8,2,1]
+b = [5,3,0,7,8,6,9,4,5]
+euclidean(a, b)
+
+def opt_euclidean(a, b):
+    d = math.sqrt(sum(math.pow((b - a), 2) for a, b in zip(a, b)))
+    print(d)
+    return d
+
+opt_euclidean(a, b)
+
+def np_euclidean(a, b):
+    d = np.sqrt(sum(np.power((b - a), 2) for a, b in zip(a, b)))
+    print(d)
+    return d
+np_euclidean(a, b)
+
